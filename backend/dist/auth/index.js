fac48e16f6433bda15f8e8739d24541aea66d2af
Edson Valença <102879625+emvalencaf@users.noreply.github.com>
2023-03-15 14:25:57 -0300

    Delete backend/dist directory

--- backend/dist/auth/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const auth_1 = require("firebase/auth");
-// firebaseapp
-const db_1 = require("../db");
-const auth = (0, auth_1.getAuth)(db_1.firebaseApp);
-exports.default = auth;
--- backend/dist/db/firestore/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const firestore_1 = require("firebase/firestore");
-const db = firestore_1.getFirestore;
-exports.default = db;
