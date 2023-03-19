62e63690a36906c2cf63a1c63b72e4be55a0fbed
emvalencaf <edsonmvf@gmail.com>
2023-03-19 18:12:22 -0300

    backend - created authGuard

--- backend/src/controllers/user/index.ts
+++ backend/src/controllers/user/index.ts
@@ -1,102 +1,135 @@
 // interface
 import { Request, Response } from "express";
 import UserRepository from "../../repository/user";
-import { IUserController, IUserControllerDeps, IUserServerResponse } from "../../shared-type/user";
+import {
+	IUserController,
+	IUserControllerDeps,
+	IUserServerResponse,
+} from "../../shared-type/user";
 
-export default class UserController{
+export default class UserController {
+	// log in an user
+	static async signIn(req: Request, res: Response) {
+		const { email, password } = req.body;
+		try {
+			const response = await UserRepository.signIn({ email, password });
 
-    // log in an user
-    static async signIn(req: Request, res: Response) {
-        const { email, password } = req.body;
-        try {
-            const response = await UserRepository.signIn({email, password});
+			if (!response)
+				return res.status(404).send({
+					data: null,
+					success: false,
+					message: "no user was found it",
+				});
 
-            if (!response) throw new Error("no ");
+			const { user } = response;
 
-            const { user } = response;
+			res.status(200).send({
+				data: user,
+				success: !!user,
+				message: user
+					? "you've successfully login"
+					: "you've failed to login",
+			});
+		} catch (err) {
+			console.log(err);
+			res.status(500).send({
+				data: null,
+				success: false,
+				message: "something went wrong on the server",
+			});
+		}
+	}
 
-            res.status(200).send({
-                data: user,
-                success: !!user,
-                message: !!user ? "you've successfully login" : "you've failed to login",
-            });
+	// register an user
+	static async signUp(req: Request, res: Response) {
+		const { name, email, password } = req.body;
+		try {
+			const responseAuth = await UserRepository.signUpAuth({
+				name,
+				email,
+				password,
+			});
 
-        } catch (err) {
-            console.log(err);
-            res.status(500).send({
-                data: null,
-                success: false,
-                message: "something went wrong on the server",
-            });
-        }
-    }
+			const response = await UserRepository.signUp(
+				{ name, email },
+				responseAuth.uid
+			);
 
-    // register an user
-    static async signUp(req: Request, res: Response) {
-        const { name, email, password } = req.body;
-        try {
-            const responseAuth = await UserRepository.signUpAuth({name, email, password});
+			res.status(201).json({
+				data: null,
+				success: true,
+				message: "your account was successufully created",
+			});
+		} catch (err) {
+			console.log(err);
+			res.status(500).json({
+				data: null,
+				success: false,
+				message: "something went wrong on the server",
+			});
+		}
+	}
 
-            const response = await UserRepository.signUp({name, email}, responseAuth.uid);
-
-            res.status(201).json({
-                data: null,
-                success: true,
-                message: "your account was successufully created",
-            })
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({
-                data: null,
-                success: false,
-                message:"something went wrong on the server",
-            });
-        }
-    }
-
-    // register an user with google
-    static async signInWithGoogle(req: Request, res: Response) {
-        const { id_token } = req.body;
-        try {
-            const responseAuth = await UserRepository.signInWithGoogleAuth
-			(id_token);
+	// register an user with google
+	static async signInWithGoogle(req: Request, res: Response) {
+		const { id_token } = req.body;
+		try {
+			const responseAuth = await UserRepository.signInWithGoogleAuth(
+				id_token
+			);
 
 			const { user } = responseAuth;
 
-            res.status(200).send({
-                data: user,
-                success: true,
-                message: "you successfully log in with google",
-            });
-        } catch (err) {
-            console.log(err);
-            res.status(500).send({
-                data: null,
-                success: false,
-                message: "server wasn't authorized to sign in with google"
-            })
-        };
-    }
+			res.status(200).send({
+				data: user,
+				success: true,
+				message: "you successfully log in with google",
+			});
+		} catch (err) {
+			console.log(err);
+			res.status(500).send({
+				data: null,
+				success: false,
+				message: "server wasn't authorized to sign in with google",
+			});
+		}
+	}
 
-    // send an email to redefine a new password
-    static async sendPasswordResetEmail(req: Request, res: Response) {
-        const { email } = req.body;
-        try {
+	// send an email to redefine a new password
+	static async sendPasswordResetEmail(req: Request, res: Response) {
+		const { email } = req.body;
+		try {
+			await UserRepository.sendPasswordResetEmail(email);
 
-            await UserRepository.sendPasswordResetEmail(email);
+			res.status(200).send({
+				data: null,
+				success: true,
+				message: "an email has been sent to the user's email",
+			});
+		} catch (err) {
+			console.log(err);
+			res.status(500).json({
+				data: null,
+				success: false,
+				message: "something went wrong on the server",
+			});
+		}
+	}
 
-            res.status(200).send({
-                data: null,
-                success: true,
-                message: "an email has been sent to the user's email",
-            });
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({
-                data: null,
-                success: false,
-                message:"something went wrong on the server",
-            });
-        }
-    }
+	// update an user profile
+	static async updateUserProfile(req: Request, res: Response) {
+		const { id } = req.params;
+		try {
+			if (id !== req.user?.uid)
+				throw new Error("you cannot try update another user's details");
+			const { name } = req.body;
+			await UserRepository.update(name);
+		} catch (err) {
+			console.log(err);
+			res.status(500).send({
+				success: false,
+				message: "something went wrong on the server",
+			});
+		}
+	}
 }
