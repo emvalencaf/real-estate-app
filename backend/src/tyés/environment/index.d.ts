94cdcc9e5c7df2450cc3374a7fac4d06065477e2
emvalencaf <edsonmvf@gmail.com>
2023-03-19 20:34:54 -0300

    type environment variables

---
+++ backend/src/tyés/environment/index.d.ts
@@ -0,0 +1,15 @@
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv extends NodeJS.ProcessEnv {
+			PORT: number;
+			DB_API_KEY: string;
+			DB_API_AUTH_DOMAIN: string;
+			DB_API_PROJECT_ID: string;
+			DB_API_STORAGE_BUCKET: string;
+			DB_API_MESSAGING_SENDER_ID: string;
+			DB_API_APP_ID: string;
+			DB_URL: string;
+			FRONTEND_URL: string;
+		}
+	}
+}
